5979def2c4f6cb6ce28c4ca6707cd4b2c5dc4a9b
Malik Ghulam Subbhani <gsmalik030@gmail.com>
2023-07-23 15:56:47 +0200

    Deleted extrat column nameed password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_23_095204) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_23_135154) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,7 +22,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_23_095204) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password"
   end
 
 end
